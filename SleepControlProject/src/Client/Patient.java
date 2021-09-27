b7019faf4825a5285cd9cc77e79100298e597ee2
gabri <gabri@10.60.58.74>
2021-09-27 11:06:15 +0200

    checking if it works

---
+++ SleepControlProject/src/Client/Patient.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author gabri
+ */
+public class Patient {
+    
+    private  String name;
+    private String lastname;
+    private String telephone;
+    private String address;
+    private Date dateOfBirth;
+    private String dni; 
+    private String gender; 
+    
+    public Patient (){
+        super();
+    }
+    
+    public Patient(String name, String lastname, String telephone, String address, Date dateOfBirth, String dni, String gender ){
+        super();
+        this.name = name;
+        this.lastname = lastname;
+        this.telephone = telephone;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        this.dni = dni;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" + "name=" + name + ", lastname=" + lastname + ", telephone=" + telephone + ", address=" + address + ", dateOfBirth=" + dateOfBirth + ", dni=" + dni + ", gender=" + gender + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.dni);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Patient other = (Patient) obj;
+        if (!Objects.equals(this.dni, other.dni)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+        
+    
+    
+    
+    
+    
+}
--- SleepControlProject/src/sleepcontrolproject/SleepControlProject.java
+++ SleepControlProject/src/sleepcontrolproject/SleepControlProject.java
@@ -17,7 +17,7 @@ public class SleepControlProject {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        int age;
+        
         
      
         
